e998d304b3b98d677a251688181fbe8fcb741a67
Lila <djyasin.ly@gmail.com>
2021-10-18 19:29:27 -0400

    added some basic CSS styling. Mapped out some more desired functionality.

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 const root = document.getElementById('root');
 const form = document.getElementById('searchBox');
-const url = "http://localhost:3000/music"
+const url = "http://127.0.0.1:8080";
+const searchResults = document.getElementById('searchResults');
+const button = document.getElementById('search')
 
 //event listener for search button 
 form.addEventListener("search", (e) => {
     e.preventDefault();
     console.log(document.getElementById('searchBox').value);
     const inputField = document.getElementById('inputField').value;
-    createMovie(inputField);
+    searchBox(inputField);
     form.reset();
 })
+
+//function searchResults(data) {
+//    fetch ('https://proxy-itunes-api.glitch.me/search?term=')
+//    .then(response => response.json())
+//   .then(data => {
+//       console.log(data)
+//       renderSongCard(data)
+//})
+
+//render the search info to a card
+//function renderSongCard
+
+//html for when the search function is called
+//searchResults.innerHTML = `
+//<p>${artistName.title}</p>
+//<img src=${artworkUrl100.image} />
+//<p>${collectionName.description}</p>
+//<p>${trackName.description}</p>
+//<audio controls>
+//<source src=${previewUrl.trackPreview} type="audio/mpeg">Play</audio>
+//`}
