f6415d8683dcbbeefb60ceebb27c2a2cffbf8451
Lila <djyasin.ly@gmail.com>
2021-10-19 16:17:43 -0400

    fleshed out the song card a bit.

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
-const root = document.getElementById('root');
-const form = document.getElementById('searchBox');
-const url = "http://127.0.0.1:8080";
-const searchResults = document.getElementById('searchResults');
-const button = document.getElementById('search')
+const root = document.getElementById("root");
+const form = document.getElementById("searchBox");
+//const url = "http://127.0.0.1:8080";
+const searchResults = document.getElementById("searchResults");
+const button = document.getElementById("search");
+const itunesProxy = "https://proxy-itunes-api.glitch.me";
 
-//event listener for search button 
+//event listener for search button
 form.addEventListener("search", (e) => {
-    e.preventDefault();
-    console.log(document.getElementById('searchBox').value);
-    const inputField = document.getElementById('inputField').value;
-    searchBox(inputField);
-    form.reset();
-})
+    console.log(document.getElementById('searchbox text').value)
+  const inputField = document.getElementById("inputField").value;
+  e.preventDefault();
+  searchBox(inputField);
+  form.reset();
+});
 
-//function searchResults(data) {
-//    fetch ('https://proxy-itunes-api.glitch.me/search?term=')
-//    .then(response => response.json())
-//   .then(data => {
-//       console.log(data)
-//       renderSongCard(data)
-//})
+function searchDisplay(data) {
+  fetch(itunesProxy + "/search?term=")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      for (let i = 0; i < data.results.length; i++) {
+        renderSongCard(data);
+      }
+    });
+}
 
 //render the search info to a card
-//function renderSongCard
-
 //html for when the search function is called
-//searchResults.innerHTML = `
-//<p>${artistName.title}</p>
-//<img src=${artworkUrl100.image} />
-//<p>${collectionName.description}</p>
-//<p>${trackName.description}</p>
-//<audio controls>
-//<source src=${previewUrl.trackPreview} type="audio/mpeg">Play</audio>
-//`}
+function renderSongCard(li, songInfo) {
+searchResults.innerHTML = `
+<p>${artistName.title}</p>
+<img src=${artworkUrl100.image} />
+<p>${collectionName.description}</p>
+<p>${trackName.description}</p>
+<audio controls>
+<source src=${previewUrl.trackPreview} type="audio/mpeg">Play</audio>
+`;
+}
